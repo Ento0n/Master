7c6c6b3dce30e283abd64cffe8940588470372f0
Ento0n <antspa@gmx.de>
2026-05-19 10:40:24 +0200

    Implement own all vs all sequence identity calculation without precomputing or cutoff.
    With that a fully connected sequence identity graph is created for extracting partitions with KaHIP.

--- data/calculate_all_vs_all_sequence_identity.py
+++ data/calculate_all_vs_all_sequence_identity.py
@@ -49,17 +49,16 @@ def calc_global_identity(seq1: str, seq2: str):
 
 
 def main():
-    seqs_df = pd.read_csv(os.path.join(DATA_DIR, "unique_sequences_with_partitioning.tsv"), sep="\t")
-
-    # filter out sequences with no partition or cluster
-    seqs_df = seqs_df[~seqs_df["partition"].isna()]
-    seqs_df = seqs_df[~seqs_df["cluster"].isna()]
+    # Load the sequences and interactions, and filter sequences to those that are in the final interactions
+    seqs_df = pd.read_csv(os.path.join(DATA_DIR, "unique_sequences.tsv"), sep="\t")
+    int_df = pd.read_csv(os.path.join(DATA_DIR, "final_filtered_interactions.tsv"), sep="\t")
+    seqs_df = seqs_df[seqs_df["new_cluster_id"].isin(int_df["new_cluster_pair"].str.split(",").explode().unique())]
 
     print(f"Calculating all vs all sequence identity for {len(seqs_df)} sequences")
 
     # get an all vs all matrix frame
     seqs = seqs_df["sequence"].tolist()
-    clusters = seqs_df["cluster"].tolist()
+    clusters = seqs_df["new_cluster_id"].tolist()
 
     local_identity_matrix = [[0.0] * len(seqs) for _ in range(len(seqs))]
     global_identity_matrix = [[0.0] * len(seqs) for _ in range(len(seqs))]
@@ -80,37 +79,22 @@ def main():
     
     # save the identity matrix to a file
     local_identity_df = pd.DataFrame(local_identity_matrix, index=clusters, columns=clusters)
-    local_identity_df.to_csv(os.path.join(os.path.join(DATA_DIR, "own_all_vs_all", "local_all_vs_all_sequence_identity.tsv")), sep="\t")
+    local_identity_df.to_csv(os.path.join(DATA_DIR, "own_all_vs_all", "local_all_vs_all_sequence_identity.tsv"), sep="\t")
     global_identity_df = pd.DataFrame(global_identity_matrix, index=clusters, columns=clusters)
-    global_identity_df.to_csv(os.path.join(os.path.join(DATA_DIR, "own_all_vs_all", "global_all_vs_all_sequence_identity.tsv")), sep="\t")
+    global_identity_df.to_csv(os.path.join(DATA_DIR, "own_all_vs_all", "global_all_vs_all_sequence_identity.tsv"), sep="\t")
 
     # Create a long format DataFrame for easier analysis
     long_format = []
-    partitions = seqs_df["partition"].tolist()
     for i in range(len(seqs)):
         for j in range(i + 1, len(seqs)):
             long_format.append({
                 "cluster1": clusters[i],
                 "cluster2": clusters[j],
-                "partition1": partitions[i],
-                "partition2": partitions[j],
                 "local_identity": local_identity_matrix[i][j],
                 "global_identity": global_identity_matrix[i][j]
             })
     long_format_df = pd.DataFrame(long_format)
-    long_format_df.to_csv(os.path.join(os.path.join(DATA_DIR, "own_all_vs_all", "all_vs_all_sequence_identity_long_format.tsv")), sep="\t", index=False)
-
-    # Save a summary per partition for average and max sequence identity between the partitions
-    summary = (
-        long_format_df.groupby(["partition1", "partition2"]).agg(
-            max_local_identity=("local_identity", "max"),
-            avg_local_identity=("local_identity", "mean"),
-            max_global_identity=("global_identity", "max"),
-            avg_global_identity=("global_identity", "mean"),
-            count=("local_identity", "count")
-        ).reset_index()
-    )
-    summary.to_csv(os.path.join(os.path.join(DATA_DIR, "own_all_vs_all", "all_vs_all_sequence_identity_summary.tsv")), sep="\t", index=False)
+    long_format_df.to_csv(os.path.join(DATA_DIR, "own_all_vs_all", "all_vs_all_sequence_identity_long_format.tsv"), sep="\t", index=False)
 
 if __name__ == "__main__":
     main()
--- data/create_graph.py
+++ data/create_graph.py
@@ -1,13 +1,16 @@
 import os
 from datetime import datetime
 from typing import Dict
+import argparse
+from itertools import combinations
+from collections import defaultdict
 
 import pandas as pd
 import networkx as nx
 
 DATA_DIR = "/nfs/scratch/pdb_dimers/"
 
-def create_metis_graph():
+def create_metis_interaction_graph():
     # Load interacion dataframe
     interaction_df = pd.read_csv(os.path.join(DATA_DIR, "final_filtered_interactions.tsv"), sep="\t")
 
@@ -56,6 +59,47 @@ def create_metis_graph():
     
     print(f"METIS graph saved to: {os.path.join(DATA_DIR, 'KaHIP', 'cluster.graph')}")
 
+def interaction_weight(pair1, pair2, seq_id_df):
+    a, b = pair1.split(",")
+    c, d = pair2.split(",")
+
+    return max(
+        seq_id_df.loc[a, c],
+        seq_id_df.loc[a, d],
+        seq_id_df.loc[b, c],
+        seq_id_df.loc[b, d]
+    )
+
+def create_metis_sequence_identity_graph():
+    int_df = pd.read_csv(os.path.join(DATA_DIR, "final_filtered_interactions.tsv"), sep="\t")
+    seq_id_df = pd.read_csv(os.path.join(DATA_DIR, "own_all_vs_all", "local_all_vs_all_sequence_identity.tsv"), sep="\t", index_col=0)
+    cluster_pairs = int_df["new_cluster_pair"]
+
+    node_id_mapping = {pair: i + 1 for i, pair in enumerate(cluster_pairs)}
+
+    # extract sequence identity for each cluster pair
+    adjacency_list = defaultdict(list)
+    for pair1, pair2 in combinations(cluster_pairs, 2):
+        weight = interaction_weight(pair1, pair2, seq_id_df)
+        int_weight = int(weight * 100)  # Convert to integer weight for METIS
+
+        i = node_id_mapping[pair1]
+        j = node_id_mapping[pair2]
+
+        adjacency_list[i].append((j, int_weight))
+        adjacency_list[j].append((i, int_weight))
+
+    n_nodes = len(cluster_pairs)
+    n_edges = sum(len(neighbors) for neighbors in adjacency_list.values()) // 2
+
+    with open(os.path.join(DATA_DIR, "MMseqs2", "sequence_identity.graph"), "w") as f:
+        f.write(f"{n_nodes} {n_edges} 001\n")  # 001 indicates weighted graph
+        for i in range(1, n_nodes + 1):
+            neighbors = adjacency_list[i]
+            for j, weight in neighbors:
+                f.write(f"{j} {weight} ")
+            f.write("\n")
+
 def create_nx_graph_from_metis():
     G = nx.Graph()
 
@@ -80,16 +124,23 @@ def create_nx_graph_from_metis():
     print(f"NetworkX graph saved to: {os.path.join(DATA_DIR, 'KaHIP', 'cluster.graph.gexf')}")
 
 def main():
+    parser = argparse.ArgumentParser(description="Create a graph from the final filtered interactions and save it in METIS format.")
+    parser.add_argument("--type", choices=["interaction", "sequence_identity"], help="Type of graph to create")
+    args = parser.parse_args()
+
     # print timestamp
     print(f"Starting graph creation at {datetime.now().isoformat(timespec='seconds')}")
 
-    create_metis_graph()
+    if args.type == "interaction":
+        create_metis_interaction_graph()
+    else:
+        create_metis_sequence_identity_graph()
 
     print(f"Graph creation completed at {datetime.now().isoformat(timespec='seconds')}")
 
-    create_nx_graph_from_metis()
-
-    print(f"NetworkX graph created from METIS file at {datetime.now().isoformat(timespec='seconds')}")
+    if args.type == "interaction":
+        create_nx_graph_from_metis()
+        print(f"NetworkX graph created from METIS file at {datetime.now().isoformat(timespec='seconds')}")
 
 if __name__ == "__main__":
     main()
