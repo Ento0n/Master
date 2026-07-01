2a2f3fb5ec8ca3042ae798d035ec3bbe025f1aa9
Ento0n <antspa@gmx.de>
2026-07-01 09:12:11 +0200

    Script to extract all interactions in a UniProt tsv format

--- data/extract_interactions.py
+++ data/extract_interactions.py
@@ -3,10 +3,16 @@ import pandas as pd
 def main():
     interactions = pd.read_csv("/nfs/scratch/pdb_dimers/final_final_filtered_interactions_with_partitions.tsv", sep="\t")
 
-    output = interactions["new_cluster_pair"].str.split(",", expand=True)
-    output.rename(columns={0:"protein1", 1:"protein2"}, inplace=True)
+    output = interactions[["uniprot_1", "uniprot_2"]].copy()
 
-    output.to_csv("/nfs/scratch/pdb_dimers/pinder/new_interactions.tsv", sep="\t", index=None)
+    output["uniprot_1"] = output["uniprot_1"].str.split(";").str[0]
+    output["uniprot_2"] = output["uniprot_2"].str.split(";").str[0]
+
+    output.rename(columns={"uniprot_1":"protein1", "uniprot_2":"protein2"}, inplace=True)
+
+    output.dropna(subset=["protein1", "protein2"], inplace=True)
+
+    output.to_csv("/nfs/scratch/pdb_dimers/pinder/pdb_interactions.tsv", sep="\t", index=None)
 
 if __name__ == "__main__":
     main()
