354db3b2b640b03245399b206c622cf349199e30
Ento0n <antspa@gmx.de>
2026-07-01 12:08:27 +0200

    fix filtering of duplicate interactions, before all duplicate entries were removed with keep=False, now keep="first" keeps one representative

--- data/filter_duplicate_interactions.py
+++ data/filter_duplicate_interactions.py
@@ -11,16 +11,18 @@ def main():
 
     # Identify duplicate interactions based on clusterpair in new_cluster_pair
     int_df["tmp_cluster_pair"] = int_df["new_cluster_pair"].apply(lambda x: ",".join(sorted(x.split(","))))
-    int_df["is_duplicate"] = int_df.duplicated(subset=["tmp_cluster_pair"], keep=False)
 
     # Filter out duplicate interactions, keeping only one representative for each cluster pair
-    filtered_int_df = int_df[~int_df["is_duplicate"]].copy()
+    filtered_int_df = int_df.drop_duplicates(
+        subset=["tmp_cluster_pair"],
+        keep="first",
+    ).copy()
 
     print(f"Number of interactions before filtering duplicates: {len(int_df)}")
     print(f"Number of interactions after filtering duplicates: {len(filtered_int_df)}")
 
     # Save the filtered interactions to a new TSV file
-    filtered_int_df.drop(columns=["tmp_cluster_pair", "is_duplicate"], inplace=True)
+    filtered_int_df.drop(columns=["tmp_cluster_pair"], inplace=True)
     filtered_int_df.to_csv(os.path.join(DATA_DIR, "final_filtered_interactions.tsv"), sep="\t", index=False)
 
 if __name__ == "__main__":
